896e7ef0becef379b3f7181d2e199d1ad93db542
AlexeyShalaev <shalaevad.alexey@yandex.ru>
2023-01-14 21:18:23 +0300

    removing duplicates v2

--- search-server/remove_duplicates.cpp
+++ search-server/remove_duplicates.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 void RemoveDuplicates(SearchServer &search_server, bool print_info = true) {
+    /*
     auto docs = search_server.GetDocuments();
     for (auto it = begin(docs); it != end(docs); it = next(it)) {
         for (auto sub_it = next(it); sub_it != end(docs); sub_it = next(sub_it)) {
@@ -19,6 +20,20 @@ void RemoveDuplicates(SearchServer &search_server, bool print_info = true) {
                 docs.erase(sub_it->first);
                 sub_it = prev_iterator;
             }
+        }
+    }
+     */
+    auto docs = search_server.GetDocuments();
+    set<set<string>> uniq_contents;
+    for (const auto &[id, doc]: search_server.GetDocuments()) {
+        if (uniq_contents.count(doc.content) == 0) {
+            uniq_contents.insert(doc.content);
+        } else {
+            if (print_info) {
+                cout << "Found duplicate document id " << id << endl;
+            }
+            search_server.RemoveDocument(id);
+            docs.erase(id);
         }
     }
 }
