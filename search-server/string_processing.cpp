6d2d87366972b0f8406c13f50913afd79e55e4e6
AlexeyShalaev <shalaevad.alexey@yandex.ru>
2023-02-18 20:58:05 +0300

    parallel policy and string_views

--- search-server/string_processing.cpp
+++ search-server/string_processing.cpp
@@ -6,22 +6,18 @@
 
 using namespace std;
 
-vector<string> SplitIntoWords(const string &text) {
-    vector<string> words;
-    string word;
-    for (const char c: text) {
-        if (c == ' ') {
-            if (!word.empty()) {
-                words.push_back(word);
-                word.clear();
-            }
+std::vector<std::string_view> SplitIntoWords(std::string_view str) {
+    std::vector<std::string_view> result;
+    str.remove_prefix(std::min(str.find_first_not_of(" "), str.size()));
+    while (!str.empty()) {
+        int64_t space = str.find(' ');
+        result.push_back(str.substr(0, space));
+        if (str.find(' ') != str.npos) {
+            str.remove_prefix(space);
+            str.remove_prefix(std::min(str.find_first_not_of(" "), str.size()));
         } else {
-            word += c;
+            str.remove_prefix(str.size());
         }
     }
-    if (!word.empty()) {
-        words.push_back(word);
-    }
-
-    return words;
+    return result;
 }
